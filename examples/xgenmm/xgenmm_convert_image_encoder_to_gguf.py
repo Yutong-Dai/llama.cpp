d80088bbc0f76b54abf64ef45582a46ff00181e3
root <chrisjtan23@gmail.com>
2024-08-22 23:49:02 +0000

    add gguf converting

--- examples/xgenmm/xgenmm_convert_image_encoder_to_gguf.py
+++ examples/xgenmm/xgenmm_convert_image_encoder_to_gguf.py
@@ -1,3 +1,8 @@
+""""
+Caution: The image tokenizer layers are from Minicpm, waiting for integration
+"""
+
+
 import os
 import re
 import torch
@@ -11,6 +16,88 @@ from transformers.models.siglip.modeling_siglip import SiglipVisionTransformer, 
 
 TEXT = "clip.text"
 VISION = "clip.vision"
+
+
+
+def get_1d_sincos_pos_embed_from_grid(embed_dim, pos):
+    """
+    embed_dim: output dimension for each position
+    pos: a list of positions to be encoded: size (M,)
+    out: (M, D)
+    """
+    assert embed_dim % 2 == 0
+    omega = np.arange(embed_dim // 2, dtype=np.float32)
+    omega /= embed_dim / 2.
+    omega = 1. / 10000 ** omega  # (D/2,)
+
+    pos = pos.reshape(-1)  # (M,)
+    out = np.einsum('m,d->md', pos, omega)  # (M, D/2), outer product
+
+    emb_sin = np.sin(out)  # (M, D/2)
+    emb_cos = np.cos(out)  # (M, D/2)
+
+    emb = np.concatenate([emb_sin, emb_cos], axis=1)  # (M, D)
+    return emb
+
+
+def get_2d_sincos_pos_embed_from_grid(embed_dim, grid):
+    assert embed_dim % 2 == 0
+
+    # use half of dimensions to encode grid_h
+    emb_h = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[0])  # (H*W, D/2)
+    emb_w = get_1d_sincos_pos_embed_from_grid(embed_dim // 2, grid[1])  # (H*W, D/2)
+
+    emb = np.concatenate([emb_h, emb_w], axis=1)  # (H*W, D)
+    return emb
+
+
+def get_2d_sincos_pos_embed(embed_dim, grid_size, cls_token=False):
+    """
+    grid_size: int of the grid height and width
+    return:
+    pos_embed: [grid_size*grid_size, embed_dim] or [1+grid_size*grid_size, embed_dim] (w/ or w/o cls_token)
+    """
+    if isinstance(grid_size, int):
+        grid_h_size, grid_w_size = grid_size, grid_size
+    else:
+        grid_h_size, grid_w_size = grid_size[0], grid_size[1]
+
+    grid_h = np.arange(grid_h_size, dtype=np.float32)
+    grid_w = np.arange(grid_w_size, dtype=np.float32)
+    grid = np.meshgrid(grid_w, grid_h)  # here w goes first
+    grid = np.stack(grid, axis=0)
+
+    grid = grid.reshape([2, 1, grid_h_size, grid_w_size])
+    pos_embed = get_2d_sincos_pos_embed_from_grid(embed_dim, grid)
+    if cls_token:
+        pos_embed = np.concatenate([np.zeros([1, embed_dim]), pos_embed], axis=0)
+    return pos_embed
+
+def _replace_name_resampler(s, v):
+    if re.match("resampler.pos_embed", s):
+        return {
+            s: v,
+            re.sub("pos_embed", "pos_embed_k", s): torch.from_numpy(get_2d_sincos_pos_embed(4096, (70, 70))),
+        }
+    if re.match("resampler.proj", s):
+        return {
+            re.sub("proj", "pos_embed_k", s): torch.from_numpy(get_2d_sincos_pos_embed(4096, (70, 70))),
+            re.sub("proj", "proj.weight", s): v.transpose(-1, -2).contiguous(),
+        }
+    if re.match("resampler.attn.in_proj_.*", s):
+        return {
+            re.sub("attn.in_proj_", "attn.q.", s): v.chunk(3, dim=0)[0],
+            re.sub("attn.in_proj_", "attn.k.", s): v.chunk(3, dim=0)[1],
+            re.sub("attn.in_proj_", "attn.v.", s): v.chunk(3, dim=0)[2],
+        }
+    return {s: v}
+
+def _replace_name_vit(s,v):
+    s = "vision_model." + s
+    if re.match("vision_model.embeddings.position_embedding", s):
+        v = v.unsqueeze(0)
+        return {s: v}
+    return {s: v}
 
 def k(raw_key: str, arch: str) -> str:
     return raw_key.format(arch=arch)
@@ -87,16 +174,15 @@ class print_time():
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--surgery_dir", type=str, default='/export/share/yutong/xgenmm/llamacpp_wd')
+    parser.add_argument("--surgery_dir", type=str)
     parser.add_argument('--version', type=str, default='siglip_kosmos_phi3_4k_instruct', help='help identify the version of the saved ckpt')
-    # options kept from llama.cpp projects
-    parser.add_argument("--use-f32", action="store_true", default=False, help="Use f32 instead of f16")
+    parser.add_argument("--use_f32", action="store_true", help="Use f32 instead of f16")
     parser.add_argument("--text-only", action="store_true", required=False,
                 help="Save a text-only model. It can't be used to encode images")
     parser.add_argument("--vision-only", action="store_true", required=False,
                 help="Save a vision-only model. It can't be used to encode texts")
-    parser.add_argument("--xgenmm-projector", help="Path to minicpmv.projector file. If specified, save an image encoder for XgenMM models.")
-    
+    parser.add_argument("--xgenmm_projector", help="Path to minicpmv.projector file. If specified, save an image encoder for XgenMM models.")
+    parser.add_argument("--xgenmm_vit", help="Path to vit file. ")
     return parser.parse_args()
 
 
@@ -104,73 +190,78 @@ if __name__ == "__main__":
     args = get_args()
     
     if args.text_only and args.vision_only:
-        print("--text-only and --image-only arguments cannot be specified at the same time.")
-        exit(1)
+        raise ValueError("--text-only and --image-only arguments cannot be specified at the same time.")
 
     if args.use_f32:
         print("WARNING: Weights for the convolution op is always saved in f16, as the convolution op in GGML does not support 32-bit kernel weights yet.")
-        
-    # possible data types
-    #   ftype == 0 -> float32
-    #   ftype == 1 -> float16
-    #
-    # map from ftype to string
+    
+    ckpt_dir = f"{args.surgery_dir}/{args.version}"
+    if args.xgenmm_projector is None:
+        args.xgenmm_projector = f"{ckpt_dir}/xgenmm.projector"
+    if args.xgenmm_vit is None:
+        args.xgenmm_vit = f"{ckpt_dir}/vision_encoder/xgenmm.vision_encoder"
+    output_dir = f"{ckpt_dir}/gguf"
+
     ftype_str = ["f32", "f16"]
 
     ftype = 1
     if args.use_f32:
         ftype = 0        
     
-    ckpt_dir = f"{args.surgery_dir}/{args.version}"
-    args.xgenmm_projector = f"ckpt_dir/xgenmm.projector"
-    
     with print_time("Loading vision encoder"):
         vision_encoder_config_path = f"{args.surgery_dir}/{args.version}/vision_encoder/config.json"
         with open(vision_encoder_config_path, 'r') as f:
             vision_config = json.load(f)
         vision_encoder_config = SiglipVisionConfig(**vision_config)
-    #     vision_encoder = SiglipVisionTransformer(vision_encoder_config)
-    #     vision_encoder_ckpt = torch.load(f'{ckpt_dir}/vision_encoder/xgenmm.vision_encoder')
-    #     vision_encoder.load_state_dict(vision_encoder_ckpt)
-    
+        # we don't have this config: "initializer_range": 0.02,
+        # print(vision_encoder_config)
+
+        vision_encoder = SiglipVisionTransformer(vision_encoder_config)
+        vision_encoder_ckpt = torch.load(f'{ckpt_dir}/vision_encoder/xgenmm.vision_encoder')
+        vision_encoder.load_state_dict(vision_encoder_ckpt)
+
     fname_middle = None
     has_text_encoder = True
     has_vision_encoder = True
-    has_xgenmm_projector = False
+    has_minicpmv_projector = False
     if args.text_only:
         fname_middle = "text-"
         has_vision_encoder = False
     elif args.xgenmm_projector is not None:
         fname_middle = "mmproj-"
         has_text_encoder = False
-        has_xgenmm_projector = True
+        has_minicpmv_projector = True
     elif args.vision_only:
         fname_middle = "vision-"
         has_text_encoder = False
     else:
         fname_middle = ""
 
-
-    output_dir = f"{ckpt_dir}/gguf"
+    # output_dir = f"{ckpt_dir}/gguf"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    output_prefix = os.path.basename(output_dir).replace("ggml_", "")
-    fname_out = os.path.join(output_dir, f"{fname_middle}model-{ftype_str[ftype]}.gguf")
     
+    output_prefix = os.path.basename(output_dir).replace("ggml_", "")  # seems not been used
+    fname_out = os.path.join(output_dir, f"{fname_middle}model-{ftype_str[ftype]}.gguf")
+
     fout = GGUFWriter(path=fname_out, arch="clip")
     fout.add_bool("clip.has_text_encoder", has_text_encoder)
     fout.add_bool("clip.has_vision_encoder", has_vision_encoder)
-    fout.add_bool("clip.has_xgenmm_projector", has_xgenmm_projector)
+    fout.add_bool("clip.has_minicpmv_projector", has_minicpmv_projector)
     fout.add_file_type(ftype)
     
     if args.text_only:
         fout.add_description("text-only CLIP model")
-    elif args.vision_only and not has_xgenmm_projector:
+    elif args.vision_only and not has_minicpmv_projector:
         fout.add_description("vision-only CLIP model")
-    elif has_xgenmm_projector:
-        fout.add_description("image encoder for XgenMM model")
+    elif has_minicpmv_projector:
+        # fout.add_description("image encoder for XgenMM model")
+        # # add projector type
+        # fout.add_string("clip.projector_type", "PerceiverResampler")
+        # change to real projector later
+        fout.add_description("image encoder for MiniCPM-V")
         # add projector type
-        fout.add_string("clip.projector_type", "PerceiverResampler")
+        fout.add_string("clip.projector_type", "resampler")
     else:
         fout.add_description("two-tower CLIP model")
 
@@ -201,7 +292,6 @@ if __name__ == "__main__":
         image_grid_pinpoints = [336, 672, 672, 336, 672, 672, 1008, 336, 336, 1008]
         fout.add_array("clip.vision.image_grid_pinpoints", image_grid_pinpoints)
         
-        
         image_mean = [0.5, 0.5, 0.5]
         image_std = [0.5, 0.5, 0.5]
         fout.add_array("clip.vision.image_mean", image_mean)
@@ -211,20 +301,113 @@ if __name__ == "__main__":
         use_gelu = "gelu" in vision_config["hidden_act"].lower()
         fout.add_bool("clip.use_gelu", use_gelu)
         
-    if has_xgenmm_projector:
+    # need to replace this part to PerceiverResampler
+    if has_minicpmv_projector:
         projector = torch.load(args.xgenmm_projector)
-        fout.add_uint32("clip.projector.input_dim", projector["input_dim"])
-        fout.add_uint32("clip.projector.output_dim", projector["output_dim"])
-        fout.add_uint32("clip.projector.num_heads", projector["num_heads"])
-        fout.add_uint32("clip.projector.num_layers", projector["num_layers"])
-        fout.add_uint32("clip.projector.hidden_dim", projector["hidden_dim"])
-        fout.add_float32("clip.projector.dropout", projector["dropout"])
-        fout.add_string("clip.projector.activation", projector["activation"])
-        fout.add_string("clip.projector.norm", projector["norm"])
-        fout.add_string("clip.projector.pooling", projector["pooling"])
-        fout.add_string("clip.projector.pooling_norm", projector["pooling_norm"])
-        fout.add_string("clip.projector.pooling_activation", projector["pooling_activation
-    
+        new_state_dict = {}
+        for k_, v_ in projector.items():
+            kvs = _replace_name_resampler(k_, v_)
+            for nk, nv in kvs.items():
+                new_state_dict[nk] = nv
+        projector = new_state_dict
+        for name, data in projector.items():
+            name = get_tensor_name(name)
+            data = data.squeeze().numpy()
+
+            n_dims = len(data.shape)
+
+            if ftype == 1:
+                if name[-7:] == ".weight" and n_dims == 2:
+                    print("  Converting to float16")
+                    data = data.astype(np.float16)
+                    ftype_cur = 1
+                else:
+                    print("  Converting to float32")
+                    data = data.astype(np.float32)
+                    ftype_cur = 0
+            else:
+                if data.dtype != np.float32:
+                    print("  Converting to float32")
+                    data = data.astype(np.float32)
+                    # ftype_cur = 0
+                ftype_cur = 0
+
+            fout.add_tensor(name, data)
+            print(f"{name} - {ftype_str[ftype_cur]} - shape = {data.shape}")
+
+        print("Projector tensors added\n")
+    # end
+
+
+    # for VIT model
+    state_dict = vision_encoder.state_dict()
+    new_state_dict = {}
+    for k_, v_ in state_dict.items():
+        kvs = _replace_name_vit(k_, v_)
+        for nk, nv in kvs.items():
+            # split in_proj_weight to q_proj_weight, k_proj_weight, v_proj_weight
+            if nk == "vision_model.head.attention.in_proj_weight":
+                dim = int(nv.shape[0] / 3)
+                nk_1 = "vision_model.head.attention.q_proj_weight"
+                nv_1 = nv[:dim, :]
+                nk_2 = "vision_model.head.attention.k_proj_weight"
+                nv_2 = nv[dim:2*dim, :]
+                nk_3 = "vision_model.head.attention.v_proj_weight"
+                nv_3 = nv[2*dim:, :]
+                new_state_dict[nk_1] = nv_1
+                new_state_dict[nk_2] = nv_2
+                new_state_dict[nk_3] = nv_3
+            # split in_proj_bias to q_proj_bias, k_proj_bias, v_proj_bias
+            elif nk == "vision_model.head.attention.in_proj_bias":
+                dim = int(nv.shape[0] / 3)
+                nk_1 = "vision_model.head.attention.q_proj_bias"
+                nv_1 = nv[:dim]
+                nk_2 = "vision_model.head.attention.k_proj_bias"
+                nv_2 = nv[dim:2*dim]
+                nk_3 = "vision_model.head.attention.v_proj_bias"
+                nv_3 = nv[2*dim:]
+                new_state_dict[nk_1] = nv_1
+                new_state_dict[nk_2] = nv_2
+                new_state_dict[nk_3] = nv_3
+            else:
+                new_state_dict[nk] = nv
+
+
+    state_dict = new_state_dict
+    for name, data in state_dict.items():
+        if should_skip_tensor(name, has_text_encoder, has_vision_encoder, has_minicpmv_projector):
+            # we don't need this
+            print(f"skipping parameter: {name}")
+            continue
+
+        name = get_tensor_name(name)
+        data = data.squeeze().numpy()
+        
+        n_dims = len(data.shape)
+
+        ftype_cur = 0
+        if n_dims == 4:
+            print(f"tensor {name} is always saved in f16")
+            data = data.astype(np.float16)
+            ftype_cur = 1
+        elif ftype == 1:
+            if name[-7:] == ".weight" and n_dims == 2:
+                print("  Converting to float16")
+                data = data.astype(np.float16)
+                ftype_cur = 1
+            else:
+                print("  Converting to float32")
+                data = data.astype(np.float32)
+                ftype_cur = 0
+        else:
+            if data.dtype != np.float32:
+                print("  Converting to float32")
+                data = data.astype(np.float32)
+                ftype_cur = 0
+
+        print(f"{name} - {ftype_str[ftype_cur]} - shape = {data.shape}")
+        fout.add_tensor(name, data)
+
     fout.write_header_to_file()
     fout.write_kv_data_to_file()
     fout.write_tensors_to_file()
